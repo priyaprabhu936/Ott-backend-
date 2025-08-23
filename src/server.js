4677a71c8332a4db0b8fd3c1fd81437e96a02122
priyaprabhu936 <asalappu143@gmail.com>
2025-08-23 12:14:34 +0530

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import movieRoutes from "./routes/movieRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend running successfully 🚀");
+});
+
+app.use("/api/movies", movieRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
