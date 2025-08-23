26a49dcacd0de6ca83bbef7cde38bf802d9509be
priyaprabhu936 <asalappu143@gmail.com>
2025-08-23 12:11:36 +0530

    Create Movie.js

---
+++ src/models/Movie.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  poster: { type: String }
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+export default Movie;
